be2e6ceae35aae83644cd65d0fd2eda27533c90e
Piotr Ryczek <p.ryczek@binarapps.com>
2020-05-17 23:24:41 +0200

    workflow fix, eslint

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,12 +1,7 @@
 import React from 'react';
 
-const App = () => {
-  return (
-    <span>
-      App
-      {process.env.REACT_APP_API_URL}
-    </span>
-  );
-}
+const App = () => (
+  <span>App</span>
+);
 
 export default App;
